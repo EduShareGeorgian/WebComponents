6b2e595ff7cc2e7f607c13b961299fb229a661b2
Todd Hiles <todd.hiles@georgiancollege.ca>
2017-05-26 17:20:26 -0400

    Refactored test to be DRY
    -factored common props into function

--- placeOfInterest/__tests__/ItemSnapshotTest.jsx
+++ placeOfInterest/__tests__/ItemSnapshotTest.jsx
@@ -2,38 +2,35 @@ import {PlaceOfInterestItem} from "../components/PlaceOfInterestItem";
 import React from "react"
 import renderer from "react-test-renderer"
 
-it('renders correctly', () => {
-  const props = {
-    name: "Chartwells",
-    placeLink: "https://subway.com/home",
-    mapLink: "https://maps.google.ca/?q=Chartwells",
-    locationId: "E109",
-    hoursDescription: "7:30am - 2:00pm",
-    hasCamera: false,
-    launchMap: jest.fn(),
-    launchPlaceDetails: jest.fn(),
-    launchCameraView: jest.fn()
-  }
-  const tree = renderer.create(
-      <PlaceOfInterestItem {...props}></PlaceOfInterestItem>
-  ).toJSON();
-  expect(tree).toMatchSnapshot("WithoutCamera");
-});
 
-it('renders correctly', () => {
-  const props = {
-    name: "Chartwells",
-    placeLink: "https://subway.com/home",
-    mapLink: "https://maps.google.ca/?q=Chartwells",
-    locationId: "E109",
-    hoursDescription: "7:30am - 2:00pm",
-    hasCamera: true,
-    launchMap: jest.fn(),
-    launchPlaceDetails: jest.fn(),
-    launchCameraView: jest.fn()
-  }
-  const tree = renderer.create(
-      <PlaceOfInterestItem {...props}></PlaceOfInterestItem>
-  ).toJSON();
-  expect(tree).toMatchSnapshot("WithCamera");
+describe('components', () => {
+  describe('PlaceOfInterestItem', () => {
+    const defaultProps = () => {
+      return {
+        name: "Chartwells",
+        placeLink: "https://subway.com/home",
+        mapLink: "https://maps.google.ca/?q=Chartwells",
+        locationId: "E109",
+        hoursDescription: "7:30am - 2:00pm",
+        hasCamera: false,
+        launchMap: jest.fn(),
+        launchPlaceDetails: jest.fn(),
+        launchCameraView: jest.fn()
+      }
+    };
+    it('renders correctly without Camera Icon', () => {
+      const tree = renderer.create(
+          <PlaceOfInterestItem {...defaultProps()}></PlaceOfInterestItem>
+      ).toJSON();
+      expect(tree).toMatchSnapshot("WithoutCamera");
+    })
+    it('renders correctly with Camera Icon', () => {
+      const props = defaultProps();
+      props.hasCamera = true;
+      const tree = renderer.create(
+          <PlaceOfInterestItem {...props}></PlaceOfInterestItem>
+      ).toJSON();
+      expect(tree).toMatchSnapshot("WithCamera");
+    });
+  });
 });
