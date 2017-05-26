412cd2f179ba7eae38e673975834055c7ceb004d
Todd Hiles <todd.hiles@georgiancollege.ca>
2017-05-26 17:00:53 -0400

    Dynamic Camera Icon and aria-hidden attribute
    -also added snapshot test that more elegantly displays the difference between the received and expected rendering of the component.

--- placeOfInterest/components/PlaceOfInterestItem.jsx
+++ placeOfInterest/components/PlaceOfInterestItem.jsx
@@ -8,12 +8,18 @@ class PlaceOfInterestItem extends React.Component {
         <li className="placeOfInterestItem ms-Grid-row">
           <div className="ms-Grid-col ms-u-sm3"><a href={this.props.placeLink}>{this.props.name}</a></div>
           <a target="_blank" href={this.props.mapLink} className="mapLink">
+            {this.props.hasCamera &&
+              <div class="foodServicesHoursRestaurant ms-Grid-col ms-u-sm1">
+                <i class="ms-Icon ms-Icon--Camera" aria-hidden="true"></i>
+              </div>
+            }
             <div className="ms-Grid-col ms-u-sm1">&nbsp;</div>
             <div className="ms-Grid-col ms-u-sm1">
-              <i className="ms-Icon ms-Icon--POI"></i>
+              <i className="ms-Icon ms-Icon--POI" aria-hidden="true"></i>
             </div>
             <div className="ms-Grid-col ms-u-sm2">{this.props.locationId}</div>
           </a>
+          <div className="ms-Grid-col ms-u-sm5">{this.props.hoursDescription}</div>
         </li>)
   }
 
