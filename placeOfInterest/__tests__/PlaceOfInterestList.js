0b3a7c44cc64eb154ae0df3170a4b172992c7ba0
Todd Hiles <todd.hiles@georgiancollege.ca>
2017-05-29 10:57:30 -0400

    Install and import office fabric DetailsList component.
    WIP for WebComponents-Issue #2 Places of interest list presentation component
    
    `npm test -- --watch`
    
     results in the following:
      FAIL  __tests__\PlaceOfInterestList.js (7.691s)
       ● Console
    
         console.error node_modules\fbjs\lib\warning.js:36
           Warning: Accessing PropTypes via the main React package is deprecated. Use the prop-types package from npm instead.
         console.error node_modules\@uifabric\utilities\lib\BaseComponent.js:216
           Exception in WithViewportComponent.componentDidMount(): TypeError: Cannot read property 'width' of undefined
               at WithViewportComponent._this._updateViewport (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\office-ui-fabric-react\lib\utilities\decorators\withViewport.js:43:48)
               at WithViewportComponent.componentDidMount (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\office-ui-fabric-react\lib\utilities\decorators\withViewport.js:73:18)
               at WithViewportComponent.obj.(anonymous function) [as componentDidMount] (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\@uifabric\utilities\lib\BaseComponent.js:202:42)
               at C:\data\githubShared\WebComponents\placeOfInterest\node_modules\react-test-renderer\lib\ReactCompositeComponent.js:265:25
               at measureLifeCyclePerf (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\react-test-renderer\lib\ReactCompositeComponent.js:75:12)
               at C:\data\githubShared\WebComponents\placeOfInterest\node_modules\react-test-renderer\lib\ReactCompositeComponent.js:264:11
               at CallbackQueue.notifyAll (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\react-test-renderer\lib\CallbackQueue.js:76:22)
               at ReactTestReconcileTransaction.close (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\react-test-renderer\lib\ReactTestReconcileTransaction.js:36:26)
               at ReactTestReconcileTransaction.closeAll (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\react-test-renderer\lib\Transaction.js:206:25)
               at ReactTestReconcileTransaction.perform (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\react-test-renderer\lib\Transaction.js:153:16)
               at batchedMountComponentIntoNode (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\react-test-renderer\lib\ReactTestMount.js:69:27)
               at ReactDefaultBatchingStrategyTransaction.perform (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\react-test-renderer\lib\Transaction.js:140:20)
               at Object.batchedUpdates (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\react-test-renderer\lib\ReactDefaultBatchingStrategy.js:62:26)
               at Object.batchedUpdates (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\react-test-renderer\lib\ReactUpdates.js:97:27)
               at Object.render [as create] (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\react-test-renderer\lib\ReactTestMount.js:125:18)
               at Object.<anonymous> (C:\data\githubShared\WebComponents\placeOfInterest\__tests__\PlaceOfInterestList.js:7:57)
               at Object.asyncFn (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\jest-jasmine2\build\jasmine-async.js:68:30)
               at resolve (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\jest-jasmine2\build\queueRunner.js:38:12)
               at mapper (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\jest-jasmine2\build\queueRunner.js:31:21)
               at Promise.resolve.then.el (C:\data\githubShared\WebComponents\placeOfInterest\node_modules\p-map\index.js:42:16)
               at process._tickCallback (internal/process/next_tick.js:109:7)

--- placeOfInterest/__tests__/PlaceOfInterestList.js
+++ placeOfInterest/__tests__/PlaceOfInterestList.js
@@ -1,5 +1,6 @@
 import renderer from "react-test-renderer"
 import React from "react"
+import {DetailsList} from "office-ui-fabric-react"
 
 describe( "Place of Interest list component", () => {
   it ("should render itself with an Office Fabric DetailsList component", () => {
