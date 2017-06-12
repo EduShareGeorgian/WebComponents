b6b65cabc8587c58a34e4e253b27041b66ad208b
Todd Hiles <todd.hiles@georgiancollege.ca>
2017-06-12 15:17:01 -0400

    Bundling with external React, ReactDOM and OfficeUiFabricReact
    Usage: 
    npm run build
    Output:
    C:\data\githubShared\WebComponents\placeOfInterest>npm run build
    
    > placeOfInterestWebComponent@0.0.1 build C:\data\githubShared\WebComponents\placeOfInterest
    > webpack --config webpack.config.js --progress --profile --colors
    
     10% building modules 4/5 modules 1 active ...st\components\PlaceOfInterestItem.tsx
    [at-loader] Using typescript@2.3.4 from typescript and "tsconfig.json" from C:\data\githubShared\WebComponents\placeOfInterest/tsconfig.json.
    
    1824ms building modules
    2ms sealing
    0ms optimizing
    0ms basic module optimization
    1ms module optimization
    1ms advanced module optimization
    0ms basic chunk optimization
    0ms chunk optimization
    0ms advanced chunk optimization
    1ms module and chunk tree optimization
    0ms module reviving
    1ms module order optimization
    0ms module id optimization
    1ms chunk reviving
    1ms chunk order optimization
    0ms chunk id optimization
    2ms hashing
    0ms module assets processing
    24ms chunk assets processing
    0ms additional chunk assets processing
    1ms recording
    0ms additional asset processing
    22ms chunk asset optimization
     94% asset optimization
    [at-loader] Checking started in a separate process...
    
    [at-loader] Ok, 0.326 sec.
    329ms asset optimization
    9ms emitting
    Hash: 5ad30beb058b73a9ec55
    Version: webpack 2.6.1
    Time: 2254ms
                     Asset     Size  Chunks             Chunk Names
        PlaceOfInterest.js  7.81 kB       0  [emitted]  main
    PlaceOfInterest.js.map  6.33 kB       0  [emitted]  main
       [3] ./index.js 45 bytes {0} [built]
            factory:24ms building:33ms = 57ms
        + 4 hidden modules

---
+++ placeOfInterest/webpack.config.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: __dirname + "/dist",
+        filename: 'PlaceOfInterest.js',
+        publicPath: '/components/'
+    },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+    },
+
+    module: {
+        // Important: The Loader Pipeline processing order is reverse of this list.  The first loader to process an input file is the last loader in this list.
+        loaders: [
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+            {
+                test: /.[tj]sx?$/,
+                loader: 'babel-loader',
+                include: path.join(__dirname, 'components'),
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
+        ]
+    },
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM",
+        "office-ui-fabric-react": "OfficeUiFabricReact"
+    }
+};
